f705f439f13fd817b2e67537992965a9f38230fa
David Pereira dos Anjos <168911003+DavidAnjos12@users.noreply.github.com>
2024-06-22 11:13:07 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+print(f"Seja muito bem vindo amigo!")
+n1 = float(input("Digite um número:"))
+n2 = float(input("Digite outro número:")) 
+
+if n1 > n2:
+  print(f"{n1}Maior")
+
+elif n1 == n2:
+  print(f"os números são iguais")
+
+else:
+  print(f"Menor")
